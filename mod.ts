0178f662485908da0d46d8d6af26091dccc2f6d4
jw397 <jw397@126.com>
2023-09-26 18:12:21 +0800

    feat: use path

--- mod.ts
+++ mod.ts
@@ -1,11 +1,16 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-async function handleRequest(_request: Request): Promise<Response> {
-  return new Response(await Deno.readTextFile("./index.html"), {
-    headers: {
-      "content-type": "text/html",
-    },
-  });
+async function handleRequest(request: Request): Promise<Response> {
+  const pathname = new URL(request.url).pathname;
+  console.log(request.url, ",", pathname);
+  if (pathname === "/") {
+    return new Response(await Deno.readTextFile("./index.html"), {
+      headers: {
+        "content-type": "text/html",
+      },
+    });
+  }
+  return new Response("Not Found", { status: 404 });
 }
 
 console.log("Listening on http://localhost:8000");
